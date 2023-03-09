88268b5182f38af9d84866ef5d7d2dae59dfd68b
Insowers <francesco.cadderi@gmail.com.com>
2023-03-09 13:38:44 +0100

    mattinata con ermes 03/09

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import { useEffect, useState } from "react";
 const App = () => {
 
   const [usersData, setUsersData] = useState([]);
-  
+
+  const URL = 'https://test-infobasic-defauLt-rtdb.europe-west1.firebasedatabase.app/users.json';
+
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const usernameInputChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
   useEffect(() => {
-    const URL = 'https://test-infobasic-defauLt-rtdb.europe-west1.firebasedatabase.app/users.json';
     const OPTIONS = {
       method: 'GET',
     }
@@ -14,51 +25,80 @@ const App = () => {
       response.json()
       .then(data => {
         const users = [];
-        const keys = Object.keys(data)
-        keys.forEach(() => {})
-        console.log(data);
+        Object.keys(data).forEach(key => users.push({...data[key], id: key}))
+          setUsersData(users)
       })
     })
     .catch
   }, [])
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    const user2Obj = {
+      userName: enteredUsername.trim(),
+      userEmail: enteredEmail.trim()
+    }
+
+    const REQUEST_ADD = {
+      method: 'POST',
+      body: JSON.stringify(user2Obj),
+      headers: {
+        "content-type":"application/json"
+      }
+    };
+    fetch(URL, REQUEST_ADD)
+    .then((response)=>{
+      console.log(response);
+    })
+    .catch((error)=>{
+      console.error(error);
+    })
+  }
 
   return <>
     <div className="container">
       <div className="row">
         <div className="col-6 p-5">
           <h3 className="pt-2">Aggiungi Utente</h3>
-          <form>
-            <div class="form-group pt-2">
-              <label className="pb-2" for="nome">Nome</label>
-              <input type="text" class="form-control" id="nome" placeholder="nome"/>
+          <form onSubmit={formSubmissionHandler}>
+            <div className="form-group pt-2">
+              <label className="pb-2" htmlFor="nome">Nome</label>
+              <input onChange={usernameInputChangeHandler} type="text" className="form-control" id="nome" placeholder="nome" value={enteredUsername}/>
             </div>
-            <div class="form-group py-2">
-              <label className="pb-2" for="cognome">Cognome</label>
-              <input type="text" class="form-control" id="cognome" placeholder="cognome"/>
+            <div className="form-group py-2">
+              <label className="pb-2" htmlFor="email">Email</label>
+              <input onChange={emailInputChangeHandler} type="text" className="form-control" id="email" placeholder="email" value={enteredEmail}/>
             </div>
-            <button type="submit" class="btn btn-primary">Aggiungi</button>
+            <button type="submit" className="btn btn-primary">Aggiungi</button>
           </form>
         </div>
         
         <div className="col-6 p-5">
         <h3 className="pt-2">Utenti Registrati</h3>
-          <table class="table">
+          <table className="table">
             <thead>
               <tr>
                 <th scope="col">Nome</th>
-                <th scope="col">Cognome</th>
+                <th scope="col">Email</th>
                 <th scope="col">Azioni</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>
-                  <button type="submit" class="btn btn-danger">Modifica</button>
-                  <button type="submit" class="btn btn-danger">Cancella</button>
-                </td>
-              </tr>
+              {
+                usersData.map((value)=>{
+                  return (
+                    <tr>
+                      <td>{value.userName}</td>
+                      <td>{value.userEmail}</td>
+                      <td>
+                        <button type="submit" className="btn btn-warning me-2">Modifica</button>
+                        <button type="submit" className="btn btn-danger">Cancella</button>
+                      </td>
+                    </tr> 
+                  )
+                })
+              }
             </tbody>
           </table>          
         </div>
